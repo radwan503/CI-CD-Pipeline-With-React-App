58cc362078962cd469298ea93833cf34b9ab8588
radwan503 <radwancse503@gmail.com>
2023-12-30 12:46:51 +0600

    update line

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Test  ada
+          Learn React Test
         </a>
       </header>
     </div>
